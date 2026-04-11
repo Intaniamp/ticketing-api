8ffff8922ffb7c923961edb01a9db01a9e3d3145
Intaniamp <taniaputri483@gmail.com>
2026-04-11 09:09:16 +0800

    deleting comment

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"log"
 
-	// Pastikan nama import ini sesuai dengan "go mod init ticketing-api"
 	"ticketing-api/docs"
 	"ticketing-api/routes"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors" // Tambah CORS biar Flutter nggak error
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	swagger "github.com/swaggo/fiber-swagger"
 )
@@ -24,8 +23,7 @@ import (
 // @name Authorization
 // @description Masukkan token dengan format: Bearer <spasi> token-anda
 func main() {
-	// --- 1. Konfigurasi Swagger Programatik (INI YANG KAMU MAKSUD) ---
-	// Ini gunanya biar info di UI Swagger muncul lengkap dan keren
+	// --- 1. Konfigurasi Swagger ---
 	docs.SwaggerInfo.Title = "Ticketing API Cinema"
 	docs.SwaggerInfo.Description = "API manajemen tiket bioskop - Project Multiplatform"
 	docs.SwaggerInfo.Version = "1.0"
@@ -39,8 +37,8 @@ func main() {
 	})
 
 	// --- 3. Middleware ---
-	app.Use(logger.New()) // Buat ngeliat log di terminal
-	app.Use(cors.New())   // PENTING: Supaya Flutter kamu nanti bisa akses API ini
+	app.Use(logger.New())
+	app.Use(cors.New())
 
 	// --- 4. Route Swagger ---
 	// Akses di: http://localhost:3000/swagger/index.html
