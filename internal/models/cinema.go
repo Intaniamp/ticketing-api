56369d5f31df8913e1278b629e1517fe7707c36e
cilikinari <cilikinari2005@gmail.com>
2026-06-06 01:16:12 +0800

    fix: delete city function

--- internal/models/cinema.go
+++ internal/models/cinema.go
@@ -4,13 +4,11 @@ package models
 type Cinema struct {
 	ID         int    `json:"id" example:"1"`
 	CinemaName string `json:"cinema_name" example:"Beachwalk XXI"`
-	CityID     int    `json:"city_id" example:"1"`
 	Address    string `json:"address" example:"Beachwalk Shopping Center Lt. 2, Jl. Pantai Kuta"`
 }
 
 // CinemaRequest digunakan khusus untuk menangkap input create/update dari user (tanpa ID)
 type CinemaRequest struct {
 	CinemaName string `json:"cinema_name" example:"Beachwalk XXI"`
-	CityID     int    `json:"city_id" example:"1"`
 	Address    string `json:"address" example:"Beachwalk Shopping Center Lt. 2, Jl. Pantai Kuta"`
 }
