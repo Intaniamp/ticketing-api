52de6f6d9034f3daced72bbec9fd35bbcef07b67
Intaniamp <taniaputri483@gmail.com>
2026-06-06 11:47:19 +0800

    add caching 24 Hour

--- routes/cinema_routes.go
+++ routes/cinema_routes.go
@@ -1,18 +1,27 @@
 package routes
 
 import (
+	"time"
+
 	"ticketing-api/internal/handlers"
 	"ticketing-api/internal/middleware"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 )
 
 func CinemaRoutes(api fiber.Router) {
 	cinema := api.Group("/cinema")
 
+	//caching
+	cinemaCache := cache.New(cache.Config{
+		Expiration:   24 * time.Hour,
+		CacheControl: true,
+	})
+
 	// Endpoint Publik (Bisa diakses user biasa tanpa login)
-	cinema.Get("/", handlers.GetAllCinemas)
-	cinema.Get("/:id", handlers.GetCinemaByID)
+	cinema.Get("/", cinemaCache, handlers.GetAllCinemas)
+	cinema.Get("/:id", cinemaCache, handlers.GetCinemaByID)
 
 	// Endpoint Khusus Admin (Wajib login dan role = admin)
 	cinema.Post("/", middleware.JWTProtected, middleware.AdminOnly, handlers.CreateCinema)
